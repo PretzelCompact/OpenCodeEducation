643cdbe2634371b3b59d333cbc41b40b95944de1
PretzelCompact <76824671+PretzelCompact@users.noreply.github.com>
2022-03-13 09:31:14 +0400

    Update MainClass.java

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,10 +1,12 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class MainClass {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         utils.Playable secondStage = new stage2.Runner();
         var br = new BufferedReader(new InputStreamReader(System.in));
         secondStage.play(br);
+        br.close();
     }
 }
